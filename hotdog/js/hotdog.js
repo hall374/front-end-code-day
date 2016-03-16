51bff2545c084dc37155dcd4b8947ab67a757611
Tim Hall <tim.hall@improvingenterprises.com>
2016-03-15 21:39:29 -0400

    Responsive slick carousel complete

---
+++ hotdog/js/hotdog.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+	// Initialize slick carousel on page load
+	$(".homepage .main-content .slick-carousel").slick({
+		dots: true,
+		arrows: false,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 1,
+		adaptiveHeight: true,
+		autoplay: true,
+		autoplaySpeed: 7000,
+	});
+});
